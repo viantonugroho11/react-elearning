6f7d4bfd0bf7810f70f3093ebbaf707c71229a12
viantonugroho <viantonugroho@student.gunadarma.ac.id>
2021-12-05 13:54:19 +0700

    fixing index admin

--- src/admin/jadwal/IndexJadwal.js
+++ src/admin/jadwal/IndexJadwal.js
@@ -4,7 +4,257 @@ export default class IndexJadwal extends Component {
   render() {
     return (
       <div>
-        
+<div className="main-container">
+  <div className="pd-ltr-20 xs-pd-20-10">
+    <div className="min-height-200px">
+      <div className="page-header">
+        <div className="row">
+          <div className="col-md-6 col-sm-12">
+            <div className="title">
+              <h4>Data Jadwal</h4>
+            </div>
+            <nav aria-label="breadcrumb" role="navigation">
+              <ol className="breadcrumb">
+                <li className="breadcrumb-item"><a href="index.html">Home</a></li>
+                <li className="breadcrumb-item active" aria-current="page">Data Jadwal</li>
+              </ol>
+            </nav>
+          </div>
+        </div>
+      </div>
+      {/* Simple Datatable start */}
+      <div className="card-box mb-30">
+        <div className="pd-20 d-flex justify-content-between">
+          <h4 className="text-blue h4">Data Jadwal</h4>
+          <a href="form-datasiswa.html" className="btn btn-sm btn-primary">+ tambah</a>
+        </div>
+        <div className="pb-20">
+          <table className="data-table table stripe hover nowrap">
+            <thead>
+              <tr>
+                <th className="table-plus datatable-nosort">Pelajaran</th>
+                <th>Kelas</th>
+                <th>Guru</th>
+                <th className="datatable-nosort">Aksi</th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr>
+                <td className="table-plus">Pelangi</td>
+                <td>Biru</td>
+                <td>Sagittarius</td>
+                <td>
+                  <div className="dropdown">
+                    <a className="btn btn-link font-24 p-0 line-height-1 no-arrow dropdown-toggle" href="#" role="button" data-toggle="dropdown">
+                      <i className="dw dw-more" />
+                    </a>
+                    <div className="dropdown-menu dropdown-menu-right dropdown-menu-icon-list">
+                      <a className="dropdown-item" href="#"><i className="dw dw-eye" /> View</a>
+                      <a className="dropdown-item" href="#"><i className="dw dw-edit2" /> Edit</a>
+                      <a className="dropdown-item" href="#"><i className="dw dw-delete-3" /> Delete</a>
+                    </div>
+                  </div>
+                </td>
+              </tr>
+              <tr>
+                <td className="table-plus">Andrea J. Cagle</td>
+                <td>30</td>
+                <td>Gemini</td>
+                <td>
+                  <div className="dropdown">
+                    <a className="btn btn-link font-24 p-0 line-height-1 no-arrow dropdown-toggle" href="#" role="button" data-toggle="dropdown">
+                      <i className="dw dw-more" />
+                    </a>
+                    <div className="dropdown-menu dropdown-menu-right dropdown-menu-icon-list">
+                      <a className="dropdown-item" href="#"><i className="dw dw-eye" /> View</a>
+                      <a className="dropdown-item" href="#"><i className="dw dw-edit2" /> Edit</a>
+                      <a className="dropdown-item" href="#"><i className="dw dw-delete-3" /> Delete</a>
+                    </div>
+                  </div>
+                </td>
+              </tr>
+              <tr>
+                <td className="table-plus">Andrea J. Cagle</td>
+                <td>20</td>
+                <td>Gemini</td>
+                <td>
+                  <div className="dropdown">
+                    <a className="btn btn-link font-24 p-0 line-height-1 no-arrow dropdown-toggle" href="#" role="button" data-toggle="dropdown">
+                      <i className="dw dw-more" />
+                    </a>
+                    <div className="dropdown-menu dropdown-menu-right dropdown-menu-icon-list">
+                      <a className="dropdown-item" href="#"><i className="dw dw-eye" /> View</a>
+                      <a className="dropdown-item" href="#"><i className="dw dw-edit2" /> Edit</a>
+                      <a className="dropdown-item" href="#"><i className="dw dw-delete-3" /> Delete</a>
+                    </div>
+                  </div>
+                </td>
+              </tr>
+              <tr>
+                <td className="table-plus">Andrea J. Cagle</td>
+                <td>30</td>
+                <td>Sagittarius</td>
+                <td>
+                  <div className="dropdown">
+                    <a className="btn btn-link font-24 p-0 line-height-1 no-arrow dropdown-toggle" href="#" role="button" data-toggle="dropdown">
+                      <i className="dw dw-more" />
+                    </a>
+                    <div className="dropdown-menu dropdown-menu-right dropdown-menu-icon-list">
+                      <a className="dropdown-item" href="#"><i className="dw dw-eye" /> View</a>
+                      <a className="dropdown-item" href="#"><i className="dw dw-edit2" /> Edit</a>
+                      <a className="dropdown-item" href="#"><i className="dw dw-delete-3" /> Delete</a>
+                    </div>
+                  </div>
+                </td>
+              </tr>
+              <tr>
+                <td className="table-plus">Andrea J. Cagle</td>
+                <td>25</td>
+                <td>Gemini</td>
+                <td>
+                  <div className="dropdown">
+                    <a className="btn btn-link font-24 p-0 line-height-1 no-arrow dropdown-toggle" href="#" role="button" data-toggle="dropdown">
+                      <i className="dw dw-more" />
+                    </a>
+                    <div className="dropdown-menu dropdown-menu-right dropdown-menu-icon-list">
+                      <a className="dropdown-item" href="#"><i className="dw dw-eye" /> View</a>
+                      <a className="dropdown-item" href="#"><i className="dw dw-edit2" /> Edit</a>
+                      <a className="dropdown-item" href="#"><i className="dw dw-delete-3" /> Delete</a>
+                    </div>
+                  </div>
+                </td>
+              </tr>
+              <tr>
+                <td className="table-plus">Andrea J. Cagle</td>
+                <td>20</td>
+                <td>Sagittarius</td>
+                <td>
+                  <div className="dropdown">
+                    <a className="btn btn-link font-24 p-0 line-height-1 no-arrow dropdown-toggle" href="#" role="button" data-toggle="dropdown">
+                      <i className="dw dw-more" />
+                    </a>
+                    <div className="dropdown-menu dropdown-menu-right dropdown-menu-icon-list">
+                      <a className="dropdown-item" href="#"><i className="dw dw-eye" /> View</a>
+                      <a className="dropdown-item" href="#"><i className="dw dw-edit2" /> Edit</a>
+                      <a className="dropdown-item" href="#"><i className="dw dw-delete-3" /> Delete</a>
+                    </div>
+                  </div>
+                </td>
+              </tr>
+              <tr>
+                <td className="table-plus">Andrea J. Cagle</td>
+                <td>18</td>
+                <td>Gemini</td>
+                <td>
+                  <div className="dropdown">
+                    <a className="btn btn-link font-24 p-0 line-height-1 no-arrow dropdown-toggle" href="#" role="button" data-toggle="dropdown">
+                      <i className="dw dw-more" />
+                    </a>
+                    <div className="dropdown-menu dropdown-menu-right dropdown-menu-icon-list">
+                      <a className="dropdown-item" href="#"><i className="dw dw-eye" /> View</a>
+                      <a className="dropdown-item" href="#"><i className="dw dw-edit2" /> Edit</a>
+                      <a className="dropdown-item" href="#"><i className="dw dw-delete-3" /> Delete</a>
+                    </div>
+                  </div>
+                </td>
+              </tr>
+              <tr>
+                <td className="table-plus">Andrea J. Cagle</td>
+                <td>30</td>
+                <td>Sagittarius</td>
+                <td>
+                  <div className="dropdown">
+                    <a className="btn btn-link font-24 p-0 line-height-1 no-arrow dropdown-toggle" href="#" role="button" data-toggle="dropdown">
+                      <i className="dw dw-more" />
+                    </a>
+                    <div className="dropdown-menu dropdown-menu-right dropdown-menu-icon-list">
+                      <a className="dropdown-item" href="#"><i className="dw dw-eye" /> View</a>
+                      <a className="dropdown-item" href="#"><i className="dw dw-edit2" /> Edit</a>
+                      <a className="dropdown-item" href="#"><i className="dw dw-delete-3" /> Delete</a>
+                    </div>
+                  </div>
+                </td>
+              </tr>
+              <tr>
+                <td className="table-plus">Andrea J. Cagle</td>
+                <td>30</td>
+                <td>Sagittarius</td>
+                <td>
+                  <div className="dropdown">
+                    <a className="btn btn-link font-24 p-0 line-height-1 no-arrow dropdown-toggle" href="#" role="button" data-toggle="dropdown">
+                      <i className="dw dw-more" />
+                    </a>
+                    <div className="dropdown-menu dropdown-menu-right dropdown-menu-icon-list">
+                      <a className="dropdown-item" href="#"><i className="dw dw-eye" /> View</a>
+                      <a className="dropdown-item" href="#"><i className="dw dw-edit2" /> Edit</a>
+                      <a className="dropdown-item" href="#"><i className="dw dw-delete-3" /> Delete</a>
+                    </div>
+                  </div>
+                </td>
+              </tr>
+              <tr>
+                <td className="table-plus">Andrea J. Cagle</td>
+                <td>30</td>
+                <td>Gemini</td>
+                <td>
+                  <div className="dropdown">
+                    <a className="btn btn-link font-24 p-0 line-height-1 no-arrow dropdown-toggle" href="#" role="button" data-toggle="dropdown">
+                      <i className="dw dw-more" />
+                    </a>
+                    <div className="dropdown-menu dropdown-menu-right dropdown-menu-icon-list">
+                      <a className="dropdown-item" href="#"><i className="dw dw-eye" /> View</a>
+                      <a className="dropdown-item" href="#"><i className="dw dw-edit2" /> Edit</a>
+                      <a className="dropdown-item" href="#"><i className="dw dw-delete-3" /> Delete</a>
+                    </div>
+                  </div>
+                </td>
+              </tr>
+              <tr>
+                <td className="table-plus">Andrea J. Cagle</td>
+                <td>30</td>
+                <td>Gemini</td>
+                <td>
+                  <div className="dropdown">
+                    <a className="btn btn-link font-24 p-0 line-height-1 no-arrow dropdown-toggle" href="#" role="button" data-toggle="dropdown">
+                      <i className="dw dw-more" />
+                    </a>
+                    <div className="dropdown-menu dropdown-menu-right dropdown-menu-icon-list">
+                      <a className="dropdown-item" href="#"><i className="dw dw-eye" /> View</a>
+                      <a className="dropdown-item" href="#"><i className="dw dw-edit2" /> Edit</a>
+                      <a className="dropdown-item" href="#"><i className="dw dw-delete-3" /> Delete</a>
+                    </div>
+                  </div>
+                </td>
+              </tr>
+              <tr>
+                <td className="table-plus">Andrea J. Cagle</td>
+                <td>30</td>
+                <td>Gemini</td>
+                <td>
+                  <div className="dropdown">
+                    <a className="btn btn-link font-24 p-0 line-height-1 no-arrow dropdown-toggle" href="#" role="button" data-toggle="dropdown">
+                      <i className="dw dw-more" />
+                    </a>
+                    <div className="dropdown-menu dropdown-menu-right dropdown-menu-icon-list">
+                      <a className="dropdown-item" href="#"><i className="dw dw-eye" /> View</a>
+                      <a className="dropdown-item" href="#"><i className="dw dw-edit2" /> Edit</a>
+                      <a className="dropdown-item" href="#"><i className="dw dw-delete-3" /> Delete</a>
+                    </div>
+                  </div>
+                </td>
+              </tr>
+            </tbody>
+          </table>
+        </div>
+      </div>
+      {/* Simple Datatable End */}
+    </div>
+    <div className="footer-wrap pd-20 mb-20 card-box">
+      Learning Management System By  Developer
+    </div>
+  </div>
+</div>
+
       </div>
     )
   }
