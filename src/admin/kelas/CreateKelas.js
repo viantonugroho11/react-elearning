192dc795e94817fc9292bc2283aec10f66f73263
viantonugroho <viantonugroho@student.gunadarma.ac.id>
2021-12-12 12:37:16 +0700

    fixing data kelas admin

--- src/admin/kelas/CreateKelas.js
+++ src/admin/kelas/CreateKelas.js
@@ -1,85 +1,128 @@
-import React, { Component } from 'react'
+import React from 'react'
 import Header from '../../compenent/Header'
 import Menu from '../../compenent/Menu'
 import SideBar from '../../compenent/SideBar'
+import axios from 'axios'
 
-export default class CreateKelas extends Component {
-  render() {
+//import hook history dari react router dom
+import { useHistory } from "react-router-dom";
+//import hook useState from react
+import { useState } from 'react';
+
+function CreateKelas() {
+    const [nama, setNama] = useState('');
+    const [tingkat, setTingkat] = useState('');
+    //state validation
+    const [validation, setValidation] = useState({});
+
+    //history
+    const history = useHistory();
+
+    //method "storePost"
+    const storePost = async (e) => {
+        e.preventDefault();
+        
+        //send data to server
+        await axios.post('http://localhost:8000/api/admin/kelas', {
+            nama_kelas: nama,
+            tingkat: tingkat,
+        })
+        .then(() => {
+
+            //redirect
+            history.push('/admin/kelas');
+
+        })
+        .catch((error) => {
+
+            //assign validation on state
+            setValidation(error.response.data);
+        })
+        
+    };
     return (
       <div>
         <Header/>
         <SideBar/>
         <Menu/>
-          <div className="main-container">
-  <div className="pd-ltr-20 xs-pd-20-10">
-    <div className="min-height-200px">
-      <div className="page-header">
-        <div className="row">
-          <div className="col-md-6 col-sm-12">
-            <div className="title">
-              <h4>Form Data Kelas</h4>
+        <div className="main-container">
+          <div className="pd-ltr-20 xs-pd-20-10">
+            <div className="min-height-200px">
+              <div className="page-header">
+                <div className="row">
+                  <div className="col-md-6 col-sm-12">
+                    <div className="title">
+                      <h4>Form Data Kelas</h4>
+                    </div>
+                    <nav aria-label="breadcrumb" role="navigation">
+                      <ol className="breadcrumb">
+                        <li className="breadcrumb-item"><a href="index.html">Home</a></li>
+                        <li className="breadcrumb-item active" aria-current="page">Form Data Kelas</li>
+                      </ol>
+                    </nav>
+                  </div>
+                </div>
+              </div>
+              {/* Default Basic Forms Start */}
+              <div className="pd-20 card-box mb-30">
+                <div className="clearfix">
+                  <div className="pull-left">
+                    <h4 className="text-blue h4">Form Kelas</h4>
+                    <p className="mb-30">Isilah data tersebut dengan benar !</p>
+                    {
+                      validation.errors &&
+                      <div className="alert alert-danger" role="alert">
+                        { validation.errors.map((error, index) => (
+                            <div key={index}>{ `${error.param} : ${error.msg}` }</div>
+                        )) }
+                      </div>
+                    }
+                  </div>
+                </div>
+                <form onSubmit={ storePost }>
+                  <div className="dropdown-divider" />
+                  <p><strong>Kelas</strong></p>
+                  <div className="form-group">
+                    <label>Nama Kelas</label>
+                    <input className="form-control" value={nama} onChange={(e) => setNama(e.target.value)} type="text" placeholder="Masukkan Nama Kelas" />
+                  </div>
+                  {/* <div className="form-group">
+                    <label>Abjat Rombongan Belajar</label>
+                    <select className="custom-select col-12">
+                      <option selected>Pilihan</option>
+                      <option value={1}>A</option>
+                      <option value={2}>B</option>
+                      <option value={3}>C</option>
+                    </select>
+                  </div> */}
+                  <div className="form-group">
+                    <label>Tingkatan</label>
+                    <select className="custom-select col-12" value={tingkat} onChange={(e) => setTingkat(e.target.value)}>
+                      <option selected>Pilihan</option>
+                      <option value={1}>I</option>
+                      <option value={2}>II</option>
+                      <option value={3}>III</option>
+                      <option value={4}>IV</option>
+                      <option value={5}>V</option>
+                      <option value={6}>VI</option>
+                    </select>
+                  </div>
+                  <div className="clearfix">
+                    <div className="pull-right">
+                      <button className="btn btn-primary btn-sm scroll-click" type="submit">Simpan</button>
+                    </div>
+                  </div>
+                </form>
+              </div>
+              {/* Input Validation End */}
             </div>
-            <nav aria-label="breadcrumb" role="navigation">
-              <ol className="breadcrumb">
-                <li className="breadcrumb-item"><a href="index.html">Home</a></li>
-                <li className="breadcrumb-item active" aria-current="page">Form Data Kelas</li>
-              </ol>
-            </nav>
+            <div className="footer-wrap pd-20 mb-20 card-box">
+              {/* DeskApp - Bootstrap 4 Admin Template By <a href="https://github.com/dropways" target="_blank">Ankit Hingarajiya</a> */}
+            </div>
           </div>
         </div>
-      </div>
-      {/* Default Basic Forms Start */}
-      <div className="pd-20 card-box mb-30">
-        <div className="clearfix">
-          <div className="pull-left">
-            <h4 className="text-blue h4">Form Kelas</h4>
-            <p className="mb-30">Isilah data tersebut dengan benar !</p>
-          </div>
-        </div>
-        <form>
-          <div className="dropdown-divider" />
-          <p><strong>Kelas</strong></p>
-          <div className="form-group">
-            <label>Nama Kelas</label>
-            <input className="form-control" type="text" placeholder="Masukkan Nama Kelas" />
-          </div>
-          <div className="form-group">
-            <label>Abjat Rombongan Belajar</label>
-            <select className="custom-select col-12">
-              <option selected>Pilihan</option>
-              <option value={1}>A</option>
-              <option value={2}>B</option>
-              <option value={3}>C</option>
-            </select>
-          </div>
-          <div className="form-group">
-            <label>Tingkatan</label>
-            <select className="custom-select col-12">
-              <option selected>Pilihan</option>
-              <option value={1}>I</option>
-              <option value={2}>II</option>
-              <option value={3}>III</option>
-              <option value={4}>IV</option>
-              <option value={5}>V</option>
-              <option value={6}>VI</option>
-            </select>
-          </div>
-          <div className="clearfix">
-            <div className="pull-right">
-              <a href="#horizontal-basic-form1" className="btn btn-primary btn-sm scroll-click" rel data-toggle="collapse" role="button">Simpan</a>
-            </div>
-          </div>
-        </form>
-      </div>
-      {/* Input Validation End */}
-    </div>
-    <div className="footer-wrap pd-20 mb-20 card-box">
-      {/* DeskApp - Bootstrap 4 Admin Template By <a href="https://github.com/dropways" target="_blank">Ankit Hingarajiya</a> */}
-    </div>
-  </div>
-</div>
 
       </div>
     )
-  }
 }
+export default CreateKelas
--- src/admin/kelas/IndexKelas.js
+++ src/admin/kelas/IndexKelas.js
@@ -1,57 +1,120 @@
-import React, { Component } from 'react'
+import React from 'react'
+import Header from '../../compenent/Header'
+import Menu from '../../compenent/Menu'
+import SideBar from '../../compenent/SideBar'
 
-export default class IndexKelas extends Component {
-  render() {
+import DataTable from 'react-data-table-component';
+
+//import hook useState dan useEffect from react
+import { useState, useEffect } from 'react';
+//import axios
+import axios from 'axios';
+function IndexKelas(){
+    //define state
+    const [posts, setPosts] = useState([]);
+    // A super simple expandable component.
+    const ExpandedComponent = ({ data }) => <pre>{JSON.stringify(data, null, 2)}</pre>;
+    //useEffect hook
+    useEffect(() => {
+
+        //panggil method "fetchData"
+        fectData();
+
+    }, []);
+
+    //function "fetchData"
+    const fectData = async () => {
+        //fetching
+        const response = await axios.get('http://localhost:8000/api/admin/kelas');
+        //get response data
+        const data = await response.data.data;
+
+        //assign response data to state "posts"
+        setPosts(data);
+    }
+
+    const datakelas = posts.map((user) => ({
+          nama:user.nama_kelas,
+          tingkat:user.tingkat,
+          jumlah:user.get_siswa_count,
+          aksi:
+          <div>
+            <a classname="btn btn-secondary btn-sm" href={"/admin/siswa/edit/"+user.id}>Edit</a><br/>
+            <a classname="btn btn-secondary btn-sm" href="/">Show</a><br/>
+            <a classname="btn btn-secondary btn-sm" href="/">Delete</a><br/>
+          </div>,
+    }));
+    
+
+    const columns = [
+      {
+          name: 'Nama',
+          selector: row => row.nama,
+          sortable: true,
+      },
+      {
+          name: 'Tingkat',
+          selector: row => row.tingkat,
+          sortable: true,
+      },
+      {
+          name: 'Jumlah Siswa',
+          selector: row => row.jumlah,
+          sortable: true,
+      },
+      {
+        name:'Aksi',
+        selector:row=>row.aksi
+      },
+    ];
     return (
       <div>
-<div className="main-container">
-  <div className="pd-ltr-20 xs-pd-20-10">
-    <div className="min-height-200px">
-      <div className="page-header">
-        <div className="row">
-          <div className="col-md-6 col-sm-12">
-            <div className="title">
-              <h4>Data Kelas</h4>
+        <Header/>
+        <SideBar/>
+        <Menu/>
+        <div className="main-container">
+          <div className="pd-ltr-20 xs-pd-20-10">
+            <div className="min-height-200px">
+              <div className="page-header">
+                <div className="row">
+                  <div className="col-md-6 col-sm-12">
+                    <div className="title">
+                      <h4>Data Kelas</h4>
+                    </div>
+                    <nav aria-label="breadcrumb" role="navigation">
+                      <ol className="breadcrumb">
+                        <li className="breadcrumb-item"><a href="index.html">Home</a></li>
+                        <li className="breadcrumb-item active" aria-current="page">Data Kelas</li>
+                      </ol>
+                    </nav>
+                  </div>
+                </div>
+              </div>
+              {/* Simple Datatable start */}
+              <div className="card-box mb-30">
+                <div className="pd-20 d-flex justify-content-between">
+                  <h4 className="text-blue h4">Data Kelas</h4>
+                  <a href="/admin/kelas/create" className="btn btn-sm btn-primary">+ tambah</a>
+                </div>
+                <div className="pb-20">
+                  <DataTable
+                  columns={columns}
+                    data={datakelas}
+                    // expandableRows
+                    pagination
+                    expandableRowsComponent={ExpandedComponent}
+                  />
+                </div>
+              </div>
+              {/* Simple Datatable End */}
             </div>
-            <nav aria-label="breadcrumb" role="navigation">
-              <ol className="breadcrumb">
-                <li className="breadcrumb-item"><a href="index.html">Home</a></li>
-                <li className="breadcrumb-item active" aria-current="page">Data Kelas</li>
-              </ol>
-            </nav>
+            <div className="footer-wrap pd-20 mb-20 card-box">
+              Learning Management System By  Developer
+            </div>
           </div>
         </div>
-      </div>
-      {/* Simple Datatable start */}
-      <div className="card-box mb-30">
-        <div className="pd-20 d-flex justify-content-between">
-          <h4 className="text-blue h4">Data Kelas</h4>
-          <a href="form-datakelas.html" className="btn btn-sm btn-primary">+ tambah</a>
-        </div>
-        <div className="pb-20">
-          <table className="data-table table stripe hover nowrap">
-            <thead>
-              <tr>
-                <th className="table-plus datatable-nosort">Nama Kelas</th>
-                <th>Abjad Rombongan Belajar</th>
-                <th>Tingkatan</th>
-                <th className="datatable-nosort">Aksi</th>
-              </tr>
-            </thead>
-            <tbody>
-              </tbody>
-          </table>
-        </div>
-      </div>
-      {/* Simple Datatable End */}
-    </div>
-    <div className="footer-wrap pd-20 mb-20 card-box">
-      Learning Management System By  Developer
-    </div>
-  </div>
-</div>
 
       </div>
     )
   }
-}
+export default IndexKelas
